377269246177e4d5d8ee51d81640aa43133dc0fd
Brian Zelip <bzelip@gmail.com>
2019-04-04 06:09:33 -0400

    TheHeader.vue: Create

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <FontAwesomeIcon :icon="icon('heart')"></FontAwesomeIcon>
+    <FontAwesomeIcon :icon="icon('laugh')"></FontAwesomeIcon>
+    <FontAwesomeIcon :icon="icon('meh')"></FontAwesomeIcon>
+  </header>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faHeart,
+  faLaughBeam,
+  faMeh
+} from "@fortawesome/free-regular-svg-icons";
+import { faDigitalOcean } from "@fortawesome/free-brands-svg-icons";
+
+export default {
+  components: {
+    FontAwesomeIcon
+  },
+  methods: {
+    icon(i) {
+      const fa = {
+        heart: faHeart,
+        laugh: faLaughBeam,
+        meh: faMeh
+      };
+      return fa[`${i}`];
+    }
+  }
+};
+</script>
